ee32b28e34d5b9a4736495831514279ddb1b70ea
namiqmamedov <namigmm@code.edu.az>
2023-11-30 23:13:20 +0400

    Initial create

---
+++ app/auctions/Listings.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import React, { useEffect, useState } from 'react'
+import AuctionCard from './AuctionCard';
+import { Auction } from '@/types';
+import AppPagination from '../components/AppPagination';
+import { getData } from '../actions/auctionActions';
+
+
+export default function Listings() {
+  const [auctions, setAuctions] = useState<Auction[]>([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  useEffect(() => {
+    getData(pageNumber).then(data => {
+        setAuctions(data.results);
+        setPageCount(data.pageCount);
+    })
+  }, [pageNumber])
+
+  if(auctions.length === 0) return <h5>Loading...</h5>
+  
+  return (
+    <>
+      <div className='grid grid-cols-4 gap-6'>
+        {auctions.map(auction => (
+            <AuctionCard auction={auction} key={auction.id} />
+        ))}
+      </div>
+      <div className='flex justify-center mt-4'>
+        <AppPagination
+          pageChanged={setPageNumber}
+          currentPage={pageNumber}
+          pageCount={pageCount}
+        />
+      </div>
+    </>
+  )
+}
