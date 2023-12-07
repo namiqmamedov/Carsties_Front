3117ee3d11f978513e52db4c10da1cd85e460d75
namiqmamedov <namigmm@code.edu.az>
2023-12-07 18:51:41 +0400

    Updates related to filters have been made

--- app/auctions/Listings.tsx
+++ app/auctions/Listings.tsx
@@ -9,6 +9,7 @@ import Filters from './Filters';
 import { useParamsStore } from '@/hooks/useParamsStore';
 import { shallow } from 'zustand/shallow';
 import qs from 'query-string'
+import EmptyFilter from '../components/EmptyFilter';
 
 
 export default function Listings() {
@@ -16,7 +17,9 @@ export default function Listings() {
   const params = useParamsStore(state => ({
       pageNumber: state.pageNumber,
       pageSize: state.pageSize,
-      searchTerm: state.searchTerm
+      searchTerm: state.searchTerm,
+      orderBy: state.orderBy,
+      filterBy: state.filterBy
   }), shallow);
 
   const setParams = useParamsStore(state => state.setParams);
@@ -33,22 +36,28 @@ export default function Listings() {
   }, [url])
 
   if(!data) return <h5>Loading...</h5>
-  
+
   return (
     <>
     <Filters  />
-      <div className='grid grid-cols-4 gap-6'>
-        {data.results.map(auction => (
-            <AuctionCard auction={auction} key={auction.id} />
-        ))}
-      </div>
-      <div className='flex justify-center mt-4'>
-        <AppPagination
-          pageChanged={setPageNumber}
-          currentPage={params.pageNumber}
-          pageCount={data.pageCount}
-        />
-      </div>
+    {data.totalCount === 0 ? (
+      <EmptyFilter showReset/>
+    ) : (
+       <>
+                <div className='grid grid-cols-4 gap-6'>
+                  {data.results.map(auction => (
+                      <AuctionCard auction={auction} key={auction.id} />
+                  ))}
+                </div>
+                <div className='flex justify-center mt-4'>
+                  <AppPagination
+                    pageChanged={setPageNumber}
+                    currentPage={params.pageNumber}
+                    pageCount={data.pageCount}
+                  />
+                </div>
+       </>
+    )}
     </>
   )
 }
